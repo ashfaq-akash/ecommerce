222f05e8c6ca2bfc4b0a2fc338f3828826e8aa80
ashfaq-akash <ashfaq.akash55@gmail.com>
2023-08-25 12:10:46 +1000

    second-commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome</h1>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} excat />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Rating({ value, text, color }) {
+  return (
+    <div className='rating'>
+      {[...Array(5)].map((_, i) => (
+        <span key={i}>
+          <i
+            style={{ color }}
+            className={
+              value >= i + 1
+                ? 'fas fa-star'
+                : value >= i + 0.5
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star'
+            }
+          ></i>
+        </span>
+      ))}
+      <span>{text && text}</span>
+    </div>
+  )
+}
+
+export default Rating
