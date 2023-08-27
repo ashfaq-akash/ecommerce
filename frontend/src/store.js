bd465ef81bf7fd2f07e2552b08e5f207f562d11e
ashfaq-akash <ashfaq.akash55@gmail.com>
2023-08-27 15:03:31 +1000

    sixth-commit

---
+++ frontend/src/store.js
@@ -0,0 +1,26 @@
+import {
+  legacy_createStore as createStore,
+  combineReducers,
+  applyMiddleware,
+} from 'redux'
+import thunk from 'redux-thunk' //Redux Thunk is a middleware that allows to write action creators that return a function instead of an action. It's useful for handling asynchronous actions, such as API calls.
+import { composeWithDevTools } from 'redux-devtools-extension' //This is a development tool for debugging Redux state and actions.
+import { productListReducers } from './reducers/productReducers'
+
+//Redux reducers reduce a set of actions (over time) into a single state.
+const reducer = combineReducers({
+  productList: productListReducers,
+}) //combineReducers to combine multiple reducer functions into a single reducer function.
+
+const initialState = {}
+
+const middleware = [thunk] //Middleware in Redux is a way to extend Redux with custom functionality.
+
+//The Redux store brings together the state, actions, and reducers that make up your app
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
